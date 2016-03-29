536f19670ed05588fcb8db8c3a8aea27f7266b43
Eric Palakovich Carr <carreric@gmail.com>
2016-03-29 08:49:40 -0400

    Added __str__ method to Todo model.

--- todomvc/server/todo/models.py
+++ todomvc/server/todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=300)
     marked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
